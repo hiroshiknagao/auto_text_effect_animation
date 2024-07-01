5fa425d26a4517b6771effd7e2b98bc73409ab8a
hiroshi nagao <hi_1205@hotmail.com>
2024-07-01 16:14:55 +0900

    javascript aint working

--- index.js
+++ index.js
@@ -3,5 +3,22 @@ const phrases = ["Be kind to yourself", "Everything is going to be alright",
   "Be patient", "Have a hope"];
 
 let phraseIndex = 0;
+let characterIndex = 0;
 
-containerEl.innerHTML = `<h1>I'm proud of you....${phrases[phraseIndex]}</h1>`;
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML =
+  `<h1>I'm proud of you....${phrases[phraseIndex].slice(0, characterIndex)}</h1>`;
+
+  if (characterIndex === phrases[phraseIndex].length) {
+    phraseIndex++;
+    phraseIndex = 0;
+  }
+
+  if (phraseIndex === phrases.length) {
+    phraseIndex = 0;
+  }
+  setTimeout(updateText, 400);
+};
